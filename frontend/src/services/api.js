13234415eecf7a5b692e0d844270fb4015c02508
Shekhu04 <gshikhar04@gmail.com>
2025-05-24 18:04:08 +0530

    .

--- frontend/src/services/api.js
+++
@@ -1,9 +0,0 @@
-// src/services/api.js
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // only if using cookies/auth
-});
-
-export default api;
