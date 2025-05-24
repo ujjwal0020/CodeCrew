32c9a12d2472c803e60916155284301d24595184
Shekhu04 <gshikhar04@gmail.com>
2025-05-24 18:31:31 +0530

    connected backend to frontend in production

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devtinder-3-frontend.onrender.com",
     credentials: true,
   })
 ); // to allow cross-origin requests
