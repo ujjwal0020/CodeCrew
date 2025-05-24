77a2fc7085aac2b29e760dba91f8f91a592f83c6
Shekhu04 <gshikhar04@gmail.com>
2025-05-24 19:10:42 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,34 +1,39 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
-const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+const app = express();
+
+// Middleware
 app.use(
   cors({
     origin: "https://devtinder-3-frontend.onrender.com",
     credentials: true,
   })
-); // to allow cross-origin requests
+);
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
-app.use("/", authRouter);
-app.use("/", profileRouter);
-app.use("/", requestRouter);
-app.use("/", userRouter);
+app.use("/auth", authRouter);
+app.use("/profile", profileRouter);
+app.use("/request", requestRouter);
+app.use("/user", userRouter);
 
+// Connect DB and start server
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(process.env.PORT, () => {
-      console.log("Server is running on port 3000");
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
